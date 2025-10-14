2d6eb6308a3deea6f500adb4c0ad1517f08c2450
You Song Hou <ysongweb3@gmail.com>
2025-10-13 21:21:56 -0400

    chore: package app as an npm module

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      formats: ['es', 'cjs']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    }
+  }
+});
